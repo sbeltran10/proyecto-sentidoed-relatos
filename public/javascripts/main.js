c0c3a1d2c3d9f49ed4f9cf7415e22dd88c87306d
sbeltran10 <you@example.com>
2017-05-22 18:06:27 -0500

    final

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,7 +4,7 @@ function init() {
     var imagenes1 = $("<div class ='row'></div>");
     var titulo = $("<div class ='row titulo'>RELATOS SALVAJES - LA PROPUESTA</div>");
     var imagenes2 = $("<div class ='row'></div>");
-    var descripcion = $("<div class ='row descripcion'><p>Bla blah</p></div>");
+    var descripcion = $("<div class ='row descripcion'><p>La siguiente es una historia interactiva del relato salvaje “La propuesta” en la cual un joven llamado Santiago va a un bar a tomarse unos tragos, a continuación, usted tendrá el control de decir cómo se desenlaza esta historia.</p></div>");
     var imagenFiscal = $("<div class='col-md-4'><div class='image-relato'><img src='images/relatos-fiscal.png' class='rounded-image'></div></div>");
     var imagenJose = $("<div class='col-md-4'><div class='image-relato'><img src='images/relatos-jose.png' class='rounded-image'></div></div>");
     var imagenMauricio = $("<div class='col-md-4'><div class='image-relato'><img src='images/relatos-mauricio.png' class='rounded-image'></div></div>");
@@ -25,11 +25,20 @@ function init() {
     containerDiv.append(titulo);
     containerDiv.append(imagenes2);
     containerDiv.append(descripcion);
+    var textoReinicio = $("<div class ='row'><div class ='col-md-12 reiniciar'>Iniciar</div></div>");
+    containerDiv.append(textoReinicio);
     containerDiv.append($('<a id="abajo-inicio" href="#" class="arrow down"></a>'));
+
+    
+
 
     $("#main").append(containerDiv);
 
     $("#abajo-inicio").click(function (e) { e.preventDefault(); loadChoice(0); return false; });
+
+    $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
 }
 
 function loadChoice(numChoice) {
@@ -64,18 +73,22 @@ function loadChoice(numChoice) {
 
         $("#current").replaceWith(containerDiv);
 
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+
         if (choice.redir1.startsWith("c")) {
-            $("#derecha").click(function (e) { e.preventDefault(); loadChoice(choice.redir1.substring(1, 2)); return false; });
+            $("#izquierda").click(function (e) { e.preventDefault(); loadChoice(choice.redir1.substring(1, 2)); return false; });
         }
         else {
-            $("#derecha").click(function (e) { e.preventDefault(); loadEnding(choice.redir2.substring(1, 2)); return false; });
+            $("#izquierda").click(function (e) { e.preventDefault(); loadEnding(choice.redir1.substring(1, 2)); return false; });
         }
 
         if (choice.redir2.startsWith("c")) {
-            $("#izquierda").click(function (e) { e.preventDefault(); loadChoice(choice.redir2.substring(1, 2)); return false; });
+            $("#derecha").click(function (e) { e.preventDefault(); loadChoice(choice.redir2.substring(1, 2)); return false; });
         }
         else {
-            $("#izquierda").click(function (e) { e.preventDefault(); loadEnding(choice.redir2.substring(1, 2)); return false; });
+            $("#derecha").click(function (e) { e.preventDefault(); loadEnding(choice.redir2.substring(1, 2)); return false; });
         }
 
     })
@@ -93,7 +106,7 @@ function loadEnding(numEnding) {
 
         var textoReinicio = $("<div class ='row'><div class ='col-md-12 reiniciar'>Reiniciar</div></div>");
         var flechaReinicio = $("<div class ='row'><div class ='col-md-12'><a id='arriba' href='#' class='arrow up'>Arriba</a></div></div>");
-        
+
         containerDiv.append(titulo);
         containerDiv.append(imagenes);
         containerDiv.append(descripcion);
@@ -102,7 +115,11 @@ function loadEnding(numEnding) {
 
         $("#current").replaceWith(containerDiv);
 
-        $("#arriba").click(function (e) { e.preventDefault(); load(); return false; });
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+
+        $("#arriba").click(function (e) { e.preventDefault(); loadChoice(0); return false; });
 
     })
 }
